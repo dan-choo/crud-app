7d1e5bcd99bdc60bb8b7ec7dad321268d712815b
Daniel Choo <danielchoo@Daniels-MacBook-Pro-3.local>
2022-05-08 20:04:32 -0400

    changed title

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'A Very Simple Spring Boot/Angular Web App';
 }
