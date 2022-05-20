9e6e50630fdc82808c38d6ec0c8e9863c3a255fc
Daniel Choo <danielchoo@Daniels-MacBook-Pro-3.local>
2022-05-20 11:40:16 -0400

    added setters

--- server/src/main/java/com/example/demo/User.java
+++ server/src/main/java/com/example/demo/User.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
-public class Name {
+
+public class User {
 
     private Integer id;
     private String userName;
     private String userEmail;
     private String userJob;
 
-    public Name (Integer id, String userName, String userEmail, String userJob) {
+    public User (Integer id, String userName, String userEmail, String userJob) {
         this.id = id;
         this.userName = userName;
         this.userEmail = userEmail;
@@ -18,15 +19,31 @@ public class Name {
         return id;
     }
 
+    public void setID(Integer id) {
+        this.id = id;
+    }
+
     public String getUserName() {
         return userName;
+    }
+
+    public void setUserName(String name) {
+        this.userName = name;
     }
 
     public String getUserEmail() {
         return userEmail;
     }
 
+    public void setUserEmail(String email) {
+        this.userEmail = email;
+    }
+
     public String getUserJobj() {
         return userJob;
+    }
+
+    public void setUserJob(String job) {
+        this.userJob = job;
     }
 }
