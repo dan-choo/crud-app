df1cd94b40b72b318f001004d1677aac623d5555
Daniel Choo <danielchoo@Daniels-MacBook-Pro-3.local>
2022-05-20 12:47:13 -0400

    connected backend to mySQL database

--- server/src/main/java/com/example/demo/User.java
+++ server/src/main/java/com/example/demo/User.java
@@ -1,12 +1,21 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private String userName;
     private String userEmail;
     private String userJob;
+
+    public User () {}
 
     public User (Integer id, String userName, String userEmail, String userJob) {
         this.id = id;
--- server/src/main/java/com/example/demo/UserController.java
+++ server/src/main/java/com/example/demo/UserController.java
@@ -4,25 +4,32 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path="/users")
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
-    @GetMapping("/names")
-    public List<User> get_all_names() {
-        return userService.getAllUsers();
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllNames() {
+        return userRepository.findAll();
     }
 
-    @PostMapping("/name")
-    public void add_user(@RequestBody User user) {
-        userService.addUser(user);
+    @PostMapping("/add")
+    public @ResponseBody String addUser(@RequestParam String name, @RequestParam String email,
+            @RequestParam String job) {
+        User user = new User();
+        user.setUserName(name);
+        user.setUserEmail(email);
+        user.setUserJob(job);
+        userRepository.save(user);
+        return "Saved";
     }
 }
